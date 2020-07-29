0b25ece5a5862ab9ef4badcc23b6170d287d8bae
Melissa <melissakheying@gmail.com>
2020-07-29 15:45:05 -0500

    render inputs using props

--- src/components/TempConverter/Input.js
+++ src/components/TempConverter/Input.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 //functional component
-export const Input = () => <p>I am an Input</p>
+export const Input = (props) => (
+  <div>
+    <label>{props.label}</label>
+    <input id={props.id} />
+  </div>
+  )
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+}
+
